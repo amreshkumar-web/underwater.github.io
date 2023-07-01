522f49255e5cdd86e4bebd0d56b74ff33d8f190d
amreshkumar-web <121627475+amreshkumar-web@users.noreply.github.com>
2023-07-01 10:54:47 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,17 @@
+let home=document.getElementById("h1");
+let about=document.getElementById("h2");
+let contact=document.getElementById("h3");
+function menu(){
+
+home.style.top="75%";
+home.style.transition="1s";
+about.style.left="85%";
+about.style.top="80%";
+about.style.transition="1s";
+contact.style.left="85%";
+contact.style.top="90%";
+contact.style.transition="1s";
+
+
+
+}
